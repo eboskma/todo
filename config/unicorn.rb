4b3788e1d873320fb079978f90fd22ba2f5433b4
Erwin Boskma <e.boskma@datarift.nl>
2012-03-23 22:41:14 +0100

    Fixed typo in unicorn config script

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,5 @@
 root = "/var/www/datarift.nl/todo/current"
-working_directory_root
+working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn_err.log"
 stdout_path "#{root}/log/unicorn.log"
